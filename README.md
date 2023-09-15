1757ad770cb7011a27dcc369aac06180110df5d6
Will <94875428+Und3fin3d@users.noreply.github.com>
2023-09-15 21:53:27 +0000

    testing first commit

--- README.md
+++
@@ -1,8 +0,0 @@
-# Chess
-Chess web based multiplayer game
-
-To play either uncomment the last line and comment the line above and open the address on another device on the lan.
-Or to play on the same address open the address on two different browsers e.g. normal chrome and incognito or chrome and edge/firefox etc
-This is to make sure the session data is different.
-
-Note the server only deletes session data 60 seconds after a disconnect so unless you change t ==60 to t==0 you'll have to delete the files the flask-session folder before execution.
--- static/script.js
+++ static/script.js
@@ -1,12 +1,17 @@
-const socketio = io();
 const sq = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h'}
+
 let drag;
 let beingDragged;
 let start;
+let shiftX
+let shiftY
+let lastX
+let lastY
+let over
 let overlay = document.createElement('div')
 document.body.appendChild(overlay);
 function init(pieces){
-  let center = document.createElement('center');
+  let center = document.createElement('div');
   let ChessBoard = document.createElement('div');
   ChessBoard.setAttribute('class','chessboard')
   if (pieces){
@@ -50,23 +55,7 @@ function init(pieces){
   center.appendChild(ChessBoard);
   document.body.appendChild(center);
 } 
-let legalmoves
-socketio.on('update',function(data){
-  legalmoves = data.moves
-  drawboard(data.board)
-  let targets = document.querySelectorAll('.target')
-  removehigh(targets,'target')
-  let square = document.getElementById(data.square)
-  square.classList.add('target')
-  let check = document.querySelectorAll('.highlight3')
-  removehigh(check,'highlight3')
-  document.getElementById(data.checks).classList.add('highlight3')
-
-})
-function POST(move){
-  socketio.emit("move", { data: move });
-}
-
+let legalmoves = ['b1c3', 'b1a3', 'g1h3', 'g1f3', 'a2a4', 'a2a3', 'b2b4', 'b2b3', 'c2c4', 'c2c3', 'd2d4', 'd2d3', 'e2e4', 'e2e3', 'f2f4', 'f2f3', 'g2g4', 'g2g3', 'h2h4', 'h2h3']
 function drawboard(board){
   for (let i = 0; i <8; i++){
     for (let j = 0; j<8; j++){
@@ -79,6 +68,98 @@ function drawboard(board){
         img.addEventListener('dragstart',function(e){
           beingDragged = e.target;
           start = e.target.parentNode.id
+        });
+        img.addEventListener('touchstart', function(e) {
+          beingDragged = img;
+          start = e.target.parentNode.id;
+          img.style.zIndex = 1;
+          shiftX = e.touches[0].clientX-(img.width/2);
+          shiftY = e.touches[0].clientY - (img.height/2);
+        });
+        img.addEventListener("touchmove", function(e) {
+          e.preventDefault();
+          lastX =e.touches[0].clientX 
+          lastY = e.touches[0].clientY
+          img.style.left=lastX-shiftX+"px";
+          img.style.top=lastY-shiftY+"px";
+          let tar = document.elementsFromPoint(lastX,lastY)[1]
+          if (tar != document.getElementById(start)){
+            if(tar.tagName=="DIV" && !tar.hasChildNodes()){
+              if(over!=null){
+                over.classList.remove('highlight')
+                over.classList.remove('highlight2')
+              }
+              over = tar
+              over.classList.add('highlight')
+            }
+            else if(tar.tagName=="IMG" && isLowerCase(tar.id) != isLowerCase(img.id)){
+              if(over!=null){
+                over.classList.remove('highlight')
+                over.classList.remove('highlight2')
+              }
+              over = tar.parentNode
+              over.classList.add('highlight2')
+            }
+          }
+        });
+        img.addEventListener("touchend", function(e) {
+          e.preventDefault()
+          img.style.zIndex = "";
+          let sq = document.elementsFromPoint(lastX,lastY)[1]
+          if (sq.tagName=="IMG"){
+            sq = sq.parentNode
+          }
+          let move = start + sq.id
+          if (isLowerCase(img.id)==colour){
+            if (img.id.toLowerCase()=='k'){
+              if(start.substring(0,1)=='e'){
+                if(sq.id.substring(0,1)=='c'){
+                  move = 'O-O-O'
+                }
+                if(sq.id.substring(0,1)=='g'){
+                  move = 'O-O'
+                }
+              }
+            }
+            if (legalmoves.includes(move)){
+              if(sq.children.length != 0){
+                if (isLowerCase(sq.children[0].id) != isLowerCase(img.id)){
+                  sq.textContent = '';
+                  sq.append(img)
+                  img.style.left = "50%"
+                  img.style.top = "50%"
+                  POST(move)
+                  over.classList.remove('highlight')
+                  over.classList.remove('highlight2')
+                }
+              }
+              else{
+                sq.append(img)
+                img.style.left = "50%"
+                img.style.top = "50%"
+                POST(move)
+                over.classList.remove('highlight')
+                over.classList.remove('highlight2')
+              }
+            }
+            else if (legalmoves.includes(move+'P')){
+              sq.innerHTML = ''
+              over.classList.remove('highlight2')
+              for (let i = 7; i >4; i--) {
+                let s = document.getElementById(sq.id.substring(0,1)+i)
+                s.innerHTML = ''
+              }
+              overlay.classList.add('overlay');
+              fillProm(['Q','N','B','R'],move)
+              sq.appendChild(promotion);
+            }
+            else{
+                img.style.top = "50%"
+                img.style.left = "50%"
+                over.classList.remove('highlight')
+                over.classList.remove('highlight2')
+            }
+          }
         });
         square.appendChild(img);
       }
@@ -93,8 +174,17 @@ function removehigh(targets,clas){
     target.classList.remove(clas);
   });
 }
+let colour = false
 init(colour)
-POST('initiation')
+let board = [['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
+             ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+             ['•', '•', '•', '•', '•', '•', '•', '•'],
+             ['•', '•', '•', '•', '•', '•', '•', '•'],
+             ['•', '•', '•', '•', '•', '•', '•', '•'],
+             ['•', '•', '•', '•', '•', '•', '•', '•'],
+             ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+             ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r']];
+drawboard(board)
 let promotion
 promotion = document.createElement('div');
 promotion.setAttribute('class','promotion');
@@ -141,7 +231,6 @@ squares.forEach(square => {
             if (isLowerCase(e.target.id) != isLowerCase(beingDragged.id)){
               square.textContent = '';
               square.append(beingDragged)
-              POST(move)
               square.classList.remove('highlight')
               square.classList.remove('highlight2')
             }
